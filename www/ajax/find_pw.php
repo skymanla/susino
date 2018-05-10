9c1364e35f5a412489920cb0b3f1adc284c116ab
skymanla <xnaudqlc1216@gmail.com>
2018-05-10 17:52:43 +0900

    v2
    find_pwd.php 소스 변경

--- www/ajax/find_pw.php
+++ www/ajax/find_pw.php
@@ -1,25 +1,29 @@
 <?
+/*
+password 찾기
+made by skymanla(Ryan)
+ */
 include_once $_SERVER['DOCUMENT_ROOT']."/lib/dbconn.php";
 
 header("Content-Type:application/json");
 
 
-//var_dump($_REQUEST);
 $sb_find_id = $conn->real_escape_string($_REQUEST['sb_id']);
 $sb_email = $conn->real_escape_string($_REQUEST['sb_email']);
 $sql = "select count(*) as cnt from sb_member where sb_id='".$sb_find_id."' and sb_email='".$sb_email."'";
 $q = $conn->query($sql);
 $cnt = $q->fetch_assoc();
 
-if(!$cnt['cnt']=='0'){
-	$charactersLength = strlen($sb_find_id);
+if(!$cnt['cnt']=='0'){//입력값이 존재하면
+	$hash_key = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";//암호화키
+	$charactersLength = strlen($hash_key);
 	$randomString = "";
 	for($i=0;$i<10;$i++){
-		$randomString .= $sb_find_id[rand(0, $charactersLength-1)];
+		$randomString .= $hash_key[rand(0, $charactersLength-1)];//랜덤 문자 생성
 	}
-	$chg_tmp_pwd = password_hash($randomString, PASSWORD_BCRYPT);
-	$sql = "update sb_member set sb_password='".$chg_tmp_pwd."' where sb_id='".$sb_find_id."'";
-	if($conn->query($sql)){
+	$chg_tmp_pwd = password_hash($randomString, PASSWORD_BCRYPT);//랜덤문자 암호화
+	$sql = "update sb_member set sb_password='".$chg_tmp_pwd."' where sb_id='".$sb_find_id."'";//업뎃하고
+	if($conn->query($sql)){//쿼리 들어가면 메일 발송
 
 		$header = "MIME-Version: 1.0\r\n";
 		//$header.= "Content-Type: multipart/mixed; charset=utf-8;format=flowed\r\n";
